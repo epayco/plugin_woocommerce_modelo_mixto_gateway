acb5ce041d555675b8b7a4ba2ba2a850ab1f9251
Ricardo Saldarriaga <40249987+RicardoSaldarriagaPayco@users.noreply.github.com>
2021-09-30 15:58:28 -0500

    feat:(se inizializa el plugin con sus funciones nativas correspondientes)

---
+++ woocommerce-gateway-epayco-gateway.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Epayco Payment Gateway
+ * Description: Epayco payment Gateway for WooCommerce
+ * Version: 5.x
+ * Author: Epayco
+ * Author URI: http://epayco.co
+ * License: LGPL 3.0
+ * Text Domain: epayco
+ * Domain Path: /lang
+ */
+
+add_action('plugins_loaded', 'woocommerce_epayco_gateway_init', 0);
+function woocommerce_epayco_gateway_init()
+{
+    if (!class_exists('WC_Payment_Gateway')) return;
+    include_once('includes/class-woocommerce-epayco-gateway.php');
+    add_filter('woocommerce_payment_gateways', 'woocommerce_epayco_gateway_add_gateway');
+}
+
+function woocommerce_epayco_gateway_add_gateway($methods)
+{
+    $methods[] = 'WC_Gateway_Epayco_gateway';
+    return $methods;
+}
